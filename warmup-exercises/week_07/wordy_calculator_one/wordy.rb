504fa0a5d102ca7c8ed4554e1b5d496edf35e0f7
anonymous-wolf <jackjeffress@gmail.com>
2015-11-12 09:56:00 +1100

    Added the raindrops warmup

--- warmup-exercises/week_07/wordy_calculator_one/wordy.rb
+++
@@ -1,6 +0,0 @@
-class Calculator
-  def ask(question)
-    matches = question.match(/What is (-?\d+) plus (-?\d+)?/)
-    matches[1].to_i + matches[2].to_i
-  end
-end
--- warmup-exercises/week_10/wordy_calculator/calculator.rb
+++
@@ -1,8 +0,0 @@
-class Calculator
-# ​
-  def ask( question )
-    matches = question.match( /What is (-?\d+) plus (-?\d+)/ )
-    result = matches[1].to_i + matches[2].to_i
-  end
-# ​
-end
